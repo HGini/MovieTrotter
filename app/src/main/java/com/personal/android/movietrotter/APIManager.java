c9e1ab47783b48385dc1c86b682ce07f2abba592
Hemangini <phgini@gmail.com>
2017-03-19 12:20:00 +0530

    Done: Make a class to access API data and handle no internet scenario.

--- app/src/main/java/com/personal/android/movietrotter/APIManager.java
+++ app/src/main/java/com/personal/android/movietrotter/APIManager.java
@@ -1,7 +1,18 @@
 package com.personal.android.movietrotter;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Hemangini on 3/17/17.
@@ -10,8 +21,14 @@ import android.widget.Toast;
 public class APIManager {
 
     // Static constants
-    private static final String URL_POPULAR_MOVIES = "http://api.themoviedb.org/3/movie/popular?api_key=" + APIKeys.AUTH_KEY_VER3;
-    private static final String URL_TOP_RATED_MOVIES = "http://api.themoviedb.org/3/movie/top_rated?api_key=" + APIKeys.AUTH_KEY_VER3;
+    private static final String TAG = APIManager.class.getSimpleName();
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private static final String URL_POPULAR_MOVIES = "http://api.themoviedb.org/3/movie/popular?api_key=" + APIKeys.KEY_VER3_AUTH;
+    private static final String URL_TOP_RATED_MOVIES = "http://api.themoviedb.org/3/movie/top_rated?api_key=" + APIKeys.KEY_VER3_AUTH;
+    private static final String RESPONSE_JSON_KEY_PAGE = "page";
+    private static final String RESPONSE_JSON_KEY_RESULTS = "results";
+    private static final String RESPONSE_JSON_KEY_TOTAL_RESULTS = "total_results";
+    private static final String RESPONSE_JSON_KEY_TOTAL_PAGES = "total_pages";
 
     // Others
     private Context context;
@@ -22,11 +39,76 @@ public class APIManager {
 
     public void getMoviesData() {
         if (Utils.isNetworkConnected(context)) {
+            Request request = new Request.Builder()
+                    .url(URL_POPULAR_MOVIES)
+                    .get()
+                    .build();
+            OkHttpClient okHttpClient = new OkHttpClient();
+            okHttpClient.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+                    Log.e(TAG, "getMoviesData: onFailure");
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    Log.v(TAG, "getMoviesData: onResponse");
+                    parseAndSaveMoviesData(response);
+                }
+            });
 
         }
         else {
             Toast.makeText(context, context.getString(R.string.no_network_pull_down_refresh),
                     Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void parseAndSaveMoviesData(Response response) {
+        if (response != null) {
+            try {
+                JSONObject responseBody = new JSONObject(response.body().string());
+                JSONArray moviesArray = responseBody.getJSONArray(RESPONSE_JSON_KEY_RESULTS);
+                ArrayList<Movie> movies = new ArrayList<>();
+                for (int i = 0; i < moviesArray.length(); i++) {
+                    JSONObject movieObj = moviesArray.getJSONObject(i);
+                    Movie movie = new Movie();
+                    if (movieObj.has(Movie.JSON_KEY_ID))
+                        movie.setId(movieObj.getInt(Movie.JSON_KEY_ID));
+                    if (movieObj.has(Movie.JSON_KEY_ADULT))
+                        movie.setAdult(movieObj.getBoolean(Movie.JSON_KEY_ADULT));
+                    if (movieObj.has(Movie.JSON_KEY_BACKDROP_PATH))
+                        movie.setBackdropPath(movieObj.getString(Movie.JSON_KEY_BACKDROP_PATH));
+                    if (movieObj.has(Movie.JSON_KEY_GENRE_IDS))
+                        movie.setGenreIDs(movieObj.getString(Movie.JSON_KEY_GENRE_IDS));
+                    if (movieObj.has(Movie.JSON_KEY_ORIGINAL_LANGUAGE))
+                        movie.setOriginalLang(movieObj.getString(Movie.JSON_KEY_ORIGINAL_LANGUAGE));
+                    if (movieObj.has(Movie.JSON_KEY_ORIGINAL_TITLE))
+                        movie.setOriginalTitle(movieObj.getString(Movie.JSON_KEY_ORIGINAL_TITLE));
+                    if (movieObj.has(Movie.JSON_KEY_OVERVIEW))
+                        movie.setOverview(movieObj.getString(Movie.JSON_KEY_OVERVIEW));
+                    if (movieObj.has(Movie.JSON_KEY_POSTER_PATH))
+                        movie.setPosterPath(movieObj.getString(Movie.JSON_KEY_POSTER_PATH));
+                    if (movieObj.has(Movie.JSON_KEY_POPULARITY))
+                        movie.setPopularity(movieObj.getDouble(Movie.JSON_KEY_POPULARITY));
+                    if (movieObj.has(Movie.JSON_KEY_RELEASE_DATE))
+                        movie.setReleaseDate(movieObj.getString(Movie.JSON_KEY_RELEASE_DATE));
+                    if (movieObj.has(Movie.JSON_KEY_VIDEO))
+                        movie.setVideo(movieObj.getBoolean(Movie.JSON_KEY_VIDEO));
+                    if (movieObj.has(Movie.JSON_KEY_VOTE_AVERAGE))
+                        movie.setVoteAverage(movieObj.getDouble(Movie.JSON_KEY_VOTE_AVERAGE));
+                    if (movieObj.has(Movie.JSON_KEY_VOTE_COUNT))
+                        movie.setVoteCount(movieObj.getInt(Movie.JSON_KEY_VOTE_COUNT));
+                    if (movieObj.has(Movie.JSON_KEY_TITLE))
+                        movie.setTitle(movieObj.getString(Movie.JSON_KEY_TITLE));
+                    movies.add(movie);
+                }
+
+            } catch (JSONException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
--- app/src/main/java/com/personal/android/movietrotter/Movie.java
+++ app/src/main/java/com/personal/android/movietrotter/Movie.java
@@ -6,5 +6,145 @@ package com.personal.android.movietrotter;
 
 public class Movie {
 
+    public static final String JSON_KEY_POSTER_PATH = "poster_path";
+    public static final String JSON_KEY_ADULT = "adult";
+    public static final String JSON_KEY_OVERVIEW = "overview";
+    public static final String JSON_KEY_RELEASE_DATE = "release_date";
+    public static final String JSON_KEY_GENRE_IDS = "genre_ids";
+    public static final String JSON_KEY_ID = "id";
+    public static final String JSON_KEY_ORIGINAL_TITLE = "original_title";
+    public static final String JSON_KEY_ORIGINAL_LANGUAGE = "original_language";
+    public static final String JSON_KEY_TITLE = "title";
+    public static final String JSON_KEY_BACKDROP_PATH = "backdrop_path";
+    public static final String JSON_KEY_POPULARITY = "popularity";
+    public static final String JSON_KEY_VOTE_COUNT = "vote_count";
+    public static final String JSON_KEY_VIDEO = "video";
+    public static final String JSON_KEY_VOTE_AVERAGE = "vote_average";
 
+    private int id;
+    private int voteCount;
+    private double voteAverage;
+    private double popularity;
+    private boolean isAdult = true;
+    private boolean isVideo = false;
+    private String posterPath;
+    private String overview;
+    private String releaseDate;
+    private String genreIDs;
+    private String originalTitle;
+    private String originalLang;
+    private String title;
+    private String backdropPath;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
+
+    public void setAdult(boolean adult) {
+        isAdult = adult;
+    }
+
+    public boolean isVideo() {
+        return isVideo;
+    }
+
+    public void setVideo(boolean video) {
+        isVideo = video;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getGenreIDs() {
+        return genreIDs;
+    }
+
+    public void setGenreIDs(String genreIDs) {
+        this.genreIDs = genreIDs;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOriginalLang() {
+        return originalLang;
+    }
+
+    public void setOriginalLang(String originalLang) {
+        this.originalLang = originalLang;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
 }
