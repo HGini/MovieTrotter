21e44eec3b8b0fb7964dcad27cd9235b10ff9cf3
Hemangini <phgini@gmail.com>
2017-03-20 13:13:58 +0530

    In progress: Details activity

---
+++ app/src/main/java/com/personal/android/movietrotter/DetailsActivity.java
@@ -0,0 +1,18 @@
+package com.personal.android.movietrotter;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hemangini on 3/20/17.
+ */
+
+public class DetailsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
